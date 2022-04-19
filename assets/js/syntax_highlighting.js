c3dd81e44d14b8c05d0e6deb3927352c6dd178af
Julia Wu <juliawu@google.com>
2022-04-19 14:07:05 -0700

    template rest api page, with css and javascript

---
+++ assets/js/syntax_highlighting.js
@@ -0,0 +1,23 @@
+/*
+Custom syntax highlighting for REST api request signatures, for use on documentation pages.
+
+Highlights text with the id "api-signature"
+Selects all instances of POST, GET, DELETE, PUT, PATCH and applies the "keyword" class to it
+Selects all instances of words surrounded by {} and applies the "param" class to it
+
+For an example on how this is used, see /api/rest/rest_api_template.md
+*/
+
+function customRESTSyntaxHighlighting(id) {
+    var signature = document.getElementById(id)
+    var html = signature.innerHTML;
+    var keyword_regex = /(GET|POST|HEAD|DELETE|PUT|PATCH)/g;
+    var param_regex = /{[A-Za-z0-9]+}/g;
+    
+    html = html.replace(keyword_regex, '<span class="keyword">$&</span>');
+    html = html.replace(param_regex, '<span class="param">$&</span>');
+    
+    signature.innerHTML = html;
+}
+
+customRESTSyntaxHighlighting("api-signature")
